9925fa9b5a8494b54f88a702a83119d014ef98bb
Nguyen Duc Thang <thang-nd@blcokhvie.vn>
2022-07-29 14:08:12 +0700

    up

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,12 +4,12 @@ const Product = ({img,link}) => {
   return (
     <div className="p">
       <div className="p-browser">
-        <div src="https://github.com/thang-data/MERN_APP_FULLSTACK.git" className="p-circle"></div>
+        <div src="" className="p-circle"></div>
         <div className="p-circle"></div>
         <div className="p-circle"></div>
       </div>
-      <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} alt="" className="p-img" />
+      <a href={link} target="_blank" rel="noreferrer" >
+        <img src={img} className="p-img" alt=""/>
       </a>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,32 +1,9 @@
+import img1 from "../src/img/anh1.png"
+
 export const products = [
   {
     id: 1,
-    img: "/src/img/68747470733a2f2f692e6962622e636f2f5a385930434a762f53637265656e73686f742d323032302d31302d33302d61742d31312d31302d30342e706e67.png",
+    img: img1,
     link: "https://github.com/thang-data/social.git",
-  },
-  {
-    id: 2,
-    img: "",
-    link: "http://lama.dev",
-  },
-  {
-    id: 3,
-    img: "",
-    link: "http://lama.dev",
-  },
-  {
-    id: 4,
-    img: "",
-    link: "http://lama.dev",
-  },
-  {
-    id: 5,
-    img: "",
-    link: "http://lama.dev",
-  },
-  {
-    id: 6,
-    img: "",
-    link: "http://lama.dev",
-  },
-];
+  }
+]
